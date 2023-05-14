261b9e7ea3ff607a13614c45362d333e8f0eb4d4
NetSe24 <netse0962@gmail.com>
2023-05-14 15:24:10 +0700

    Create tables

---
+++ database/migrations/2023_05_13_064959_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_type');
+
+            // relationship one to many between event and stadium
+            $table->unsignedBigInteger('stadium_id');
+            $table->foreign('stadium_id')
+                ->references('id')
+                ->on('stadiums')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
---
+++ database/migrations/2023_05_14_064940_create_competitions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('competitions', function (Blueprint $table) {
+            $table->id();
+            $table->string('matching_team');
+            $table->string('location');
+
+            // one to many relationships between event and competition
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')
+                ->references('id')
+                ->on('events')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('competitions');
+    }
+};
